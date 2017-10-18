78d9174990adb7b66a5355d73b68f5361fe0513e
Build a better bonsai <neumann6994@gmail.com>
2017-10-18 12:41:01 -0500

    added text

--- Main.cpp
+++ Main.cpp
@@ -9,19 +9,21 @@ int main(int argc, char** argv)
 	int screen = 0;
 
 	//Window creation
-	sf::RenderWindow App(sf::VideoMode(640, 480, 32), "SFML Demo 3");
+	sf::RenderWindow App(sf::VideoMode(1500, 1500, 32), "TowerCrawl");
 
 	//Mouse cursor no more visible
-	App.setMouseCursorVisible(false);
+	App.setMouseCursorVisible(true);
 
-	//Screens preparations
-	screen_Main s0;
-	Screens.push_back(&s0);
-	screen_Inventory s1;
-	Screens.push_back(&s1);
+	//Screens preparations -----comment out all but the one you are working on to test
+	//screen_Main sM; 
+	//Screens.push_back(&sM);
+	screen_Battle sB;
+	Screens.push_back(&sB);
+	//screen_Inventory sI;
+	//Screens.push_back(&sI);
 
 	//Main loop
-	while (screen >= 0)
+	while (screen >= 0)//screens will run in the order they are arrranged
 	{
 		screen = Screens[screen]->Run(App);
 	}
--- screen_Battle.cpp
+++ screen_Battle.cpp
@@ -10,6 +10,57 @@ screen_Battle::screen_Battle(void)
 
 int screen_Battle::Run(sf::RenderWindow &App)
 {
-	//NEUMANN ASSIGNED TO FUNCTIONALITY 
+	playing = true;
+	sf::CircleShape shape(50.f,10);
+	shape.setFillColor(sf::Color::Red);
+	App.setTitle("BATTLE!");
+	sf::Font font;
+	if (!font.loadFromFile("OpenSans-Regular.ttf"))
+	{
+		// error...
+	}
+	sf::Text Menu1;
+	sf::Text Menu2;
+	sf::Text Menu3;
+
+	//Menu1.setFont(sf::Font());
+	Menu1.setFont(font);
+	Menu1.setCharacterSize(20);
+	Menu1.setString("Play");
+	Menu1.setPosition({ 280.f, 160.f });
+
+	//Menu2.setFont(sf::Font());
+	Menu2.setCharacterSize(20);
+	Menu2.setString("Exit");
+	Menu2.setPosition({ 280.f, 220.f });
+
+	//Menu3.setFont(sf::Font());
+	Menu3.setCharacterSize(20);
+	Menu3.setString("Continue");
+	Menu3.setPosition({ 280.f, 160.f });
+
+	sf::Event event;
+	while (App.isOpen())
+	{
+		//shape.move(1, 1);
+		//if (newRoom) { LoadRoom(); newRoom = false; }// Loads new room values 
+
+		while (App.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				App.close();
+			//bool ismove = HandleInput();
+		}
+
+
+
+		App.clear(sf::Color::White);
+		App.draw(Menu1);
+		App.draw(Menu2);
+		//App.draw(shape);
+		App.display();
+
+	}
+
 	return 0;
 }
